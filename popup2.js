3f6770106ecc6432dfd79fa2f882f6e8990656ff
Nijiko Suryadikara <59920622+nijikos@users.noreply.github.com>
2024-06-01 14:37:37 +0700

    popup2

---
+++ popup2.js
@@ -0,0 +1,41 @@
+window.addEventListener(
+  "message",
+  (event) => {
+    const { origin, data } = event;
+
+    if (origin === "http://54.254.188.38:3333") {
+      console.log("Received message from allowed origin:", { origin, data });
+
+      const platterPopupIframe = document.getElementById(
+        "platter-popup-non-kms"
+      );
+      if (!platterPopupIframe) {
+        console.error("Iframe with id 'platter-popup-non-kms' not found.");
+        return;
+      }
+
+      platterPopupIframe.style.position = "absolute";
+      platterPopupIframe.style.bottom = "0px";
+      platterPopupIframe.style.right = "0px";
+
+      const { height, width } = data;
+
+      if (height === "80px") {
+        platterPopupIframe.style.transition = "none";
+        console.log("Transition set to none for height 80px.");
+      } else {
+        platterPopupIframe.style.transition = "all 300ms";
+        console.log("Transition set to 300ms.");
+      }
+
+      setTimeout(() => {
+        platterPopupIframe.style.height = height;
+        platterPopupIframe.style.width = width;
+        console.log(`Iframe resized to height: ${height}, width: ${width}`);
+      }, 200);
+    } else {
+      console.warn("Message received from untrusted origin:", origin);
+    }
+  },
+  false
+);
